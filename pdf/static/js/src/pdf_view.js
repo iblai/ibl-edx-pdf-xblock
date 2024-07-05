289a1a10d2ec8afff6e9b83d8446e2c40fe59c5d
gipsbrian <gipsbrian@gmail.com>
2024-07-05 15:18:28 +0000

    - update to student_view

--- pdf/static/js/src/pdf_view.js
+++ pdf/static/js/src/pdf_view.js
@@ -8,13 +8,10 @@ function loadPDF(href) {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
             // Assuming the response is a direct PDF link or PDF data
             // Adjust this part based on the actual response format
-            var pdfContainer = document.getElementById('nunpai1');
-            // Example: Insert an <iframe> or <object> tag to display the PDF
-            pdfContainer.innerHTML = '<iframe src="' + xmlHttp.responseText + '" width="100%" height="600px"></iframe>';
+            var iframe = document.getElementById('unit-iframe');
+            // Set the src of the iframe to the PDF link or data received from the response
+            iframe.src = xmlHttp.responseText;
         }
     }
     xmlHttp.send(parameters);
 }
-
-// Since the href is dynamically rendered, you'll need to pass it to loadPDF
-// This can be done directly in the HTML template or via another script tag that initializes this function
