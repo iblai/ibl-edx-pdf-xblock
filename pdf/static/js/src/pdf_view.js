323f22d8ae20f55a5343c4381013549689cdbb17
gipsbrian <gipsbrian@gmail.com>
2024-07-05 15:23:08 +0000

    - update to student_view

--- pdf/static/js/src/pdf_view.js
+++ pdf/static/js/src/pdf_view.js
@@ -6,10 +6,7 @@ function loadPDF(href) {
     xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            // Assuming the response is a direct PDF link or PDF data
-            // Adjust this part based on the actual response format
             var iframe = document.getElementById('unit-iframe');
-            // Set the src of the iframe to the PDF link or data received from the response
             iframe.src = xmlHttp.responseText;
         }
     }
