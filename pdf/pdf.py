5aadd17f403ffbce699acd50b0376ce0ba16efa4
gipsbrian <gipsbrian@gmail.com>
2024-07-09 15:55:34 +0000

    - pass href to studio context

--- pdf/pdf.py
+++ pdf/pdf.py
@@ -72,7 +72,7 @@ class pdfXBlock(XBlock):
         frag = Fragment(html.format(self=self))
         frag.add_javascript(self.resource_string("static/js/src/pdf_edit.js"))
         frag.add_javascript(self.resource_string("static/js/src/pdf_view.js"))
-        frag.initialize_js('pdfXBlock')
+        frag.initialize_js('pdfXBlock', {"href": self.href})
         return frag
 
     # def studio_view(self, context=None):
